b2f94908619cae48ffe39d3e397cf6197c047d94
tamagon0908 <takahashi090814@gmail.com>
2026-06-22 17:31:35 +0900

    seprate folder

--- firmware/ctrl/.pio/build/freenove_esp32_wrover/libb43/SCServo/SCS.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/freenove_esp32_wrover/libb43/SCServo/SCS.cpp.o: \
- .pio/libdeps/freenove_esp32_wrover/SCServo/src/SCS.cpp \
- .pio/libdeps/freenove_esp32_wrover/SCServo/src/SCS.h \
- .pio/libdeps/freenove_esp32_wrover/SCServo/src/INST.h
